e859c90f913209ebca899620168d61d9b79f4f38
Sumanth R Dixit <111525498+sumanth-dixit94@users.noreply.github.com>
2022-08-18 22:20:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
 
-streamlit.text("Bio- Data");
+streamlit.title("Bio- Data");
 streamlit.header("Name - Sumanth Dixit");
 streamlit.text("I reside in India, age is 28 and I love snowflake");
