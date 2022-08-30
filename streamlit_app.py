ae86bb2c2170797084a19bf37f0e8f8cc2c9b112
Sumanth R Dixit <111525498+sumanth-dixit94@users.noreply.github.com>
2022-08-30 16:41:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import streamlit
 streamlit.title("Bio- Data");
 streamlit.header("Name - Sumanth Dixit");
 streamlit.text("I reside in India, age is 28 and I love snowflake");
-   🍞
-streamlit.title('Food');
-streamlit.header('~ Breakfast Menu');
+   
+streamlit.title('My moms new healthy diner');
+streamlit.header(' Breakfast Menu ');
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal');
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie');
 streamlit.text('🐔Hard-Boiled Free-Range Egg');
-streamlit.text('🥑Avocado Toast');
+streamlit.text('🥑🍞Avocado Toast');
