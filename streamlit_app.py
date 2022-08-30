7701d1ebb554e9fcace97fafbaf4e8db579dd0dc
Sumanth R Dixit <111525498+sumanth-dixit94@users.noreply.github.com>
2022-08-30 16:41:01 +0530

    added smileys

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import streamlit
 streamlit.title("Bio- Data");
 streamlit.header("Name - Sumanth Dixit");
 streamlit.text("I reside in India, age is 28 and I love snowflake");
-
+   🍞
 streamlit.title('Food');
-streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard-Boiled Free-Range Egg');
+streamlit.header('~ Breakfast Menu');
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal');
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie');
+streamlit.text('🐔Hard-Boiled Free-Range Egg');
+streamlit.text('🥑Avocado Toast');
