493e68489949cca70701c72c1fc12235adf50fe0
Sumanth R Dixit <111525498+sumanth-dixit94@users.noreply.github.com>
2022-09-12 21:57:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
 
 streamlit.title("Bio- Data");
 streamlit.header("Name - Sumanth Dixit");
@@ -33,3 +32,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
